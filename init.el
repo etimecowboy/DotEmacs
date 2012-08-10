089feff794376a9b16e137513ecabcd25631c201
Xin Yang <Xin2.Yang@gmail.com>
2012-08-11 00:12:43 +0800

    Add `measure-time' function to measure loading time

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;   -*- mode: emacs-lisp; coding: utf-8-unix  -*-
 ;;--------------------------------------------------------------------
 ;; File name:    `init.el'
-;; Time-stamp: <2012-08-10 Fri 22:54 by xin on XIN-PC>
+;; Time-stamp: <2012-08-11 Sat 00:10 by xin on XIN-PC>
 ;; Author:       Xin Yang
 ;; Email:        xin2.yang@gmail.com
 ;; Depend on:    None
@@ -20,21 +20,23 @@
 
 (message "* ---[ Loading my Emacs init file ]---")
 
-;; (defconst emacs-load-time-start (float-time))
-;; (defvar emacs-last-time emacs-load-time-start)
-;; (defun measure-time (txt)
-;;   "*Emacs Time"
-;;   (message "* ---[ Emacs %s loaded in %1f seconds, \
-;; %1f seconds elapsed ]---"
-;;            txt
-;;            (- (float-time) emacs-last-time)
-;;            (- (float-time) emacs-load-time-start))
+(defconst start-time (float-time))
+(defvar last-time start-time)
+(defun measure-time (txt)
+  "*Emacs Time"
+  (message "* @@@[ %s loaded in %1f seconds, \
+%1f seconds elapsed ]@@@"
+           txt
+           (- (float-time) last-time)
+           (- (float-time) start-time))
 
-;;   (when (and nil (eq system-type 'windows-nt))
-;;     (with-temp-file (concat (getenv "TEMP") "/runningep.flg")
-;;       (insert "Loaded ")
-;;       (insert txt)))
-;;   (setq emacs-last-time (float-time)))
+  (when (and nil (eq system-type 'windows-nt))
+    (with-temp-file (concat (getenv "TEMP") "/runningep.flg")
+      (insert "Loaded ")
+      (insert txt)))
+  (setq last-time (float-time)))
+
+(measure-time "init.el")
 
 ;; measure the loading time per file.
 ;; (defadvice load-time (around load-with-time-logging)
@@ -52,45 +54,58 @@
 ;; Load environment settings
 ;; (require 'xy-rcroot-env)
 (load "xy-rcroot-env")
+(measure-time "xy-rcroot-env.el")
 
 ;; Emacs apparence
 ;; (require 'xy-rcroot-app)
 (load "xy-rcroot-app")
+(measure-time "xy-rcroot-app.el")
 
 ;; Some enhancements
 ;;(require 'xy-rcroot-enhance)
 (load "xy-rcroot-enhance")
+(measure-time "xy-rcroot-enhance.el")
 
 ;; Editing settings
 ;; (require 'xy-rcroot-edit)
 (load "xy-rcroot-edit")
+(measure-time "xy-rcroot-edit.el")
 
 ;; Various abbrevations and auto-completions
 ;; (require 'xy-rcroot-complete)
 (load "xy-rcroot-complete")
+(measure-time "xy-rcroot-complete.el")
 
 ;; Viewing/Comparing files
 ;; (require 'xy-rcroot-view)
 (load "xy-rcroot-view")
+(measure-time "xy-rcroot-view.el")
 
 ;; BUG: cause AC out of order.
 ;; File management
 ;; (require 'xy-rcroot-filemgr)
 (load "xy-rcroot-filemgr")
+(measure-time "xy-rcroot-filemgr.el")
 
 ;; Web application packages
 ;; (require 'xy-rcroot-net)
 (load "xy-rcroot-net")
+(measure-time "xy-rcroot-net.el")
 
 ;; Various programming settings
 ;; (require 'xy-rcroot-prog)
 (load "xy-rcroot-prog")
+(measure-time "xy-rcroot-prog.el")
 
 ;; Document packages
 ;; (require 'xy-rcroot-write)
 (load "xy-rcroot-write")
+(measure-time "xy-rcroot-write.el")
 
 ;; Save editor information between emacs sessions
 ;; Must be put at last to avoid failing
 ;; (require 'xy-rcroot-session)
 (load "xy-rcroot-session")
+(measure-time "xy-rcroot-session.el")
+
+(measure-time "init.el")
