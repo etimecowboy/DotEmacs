04a5635dbcdd78199b97a775fee1eb8eb401bcac
Xin Yang <xin@mint>
2012-08-12 22:58:44 +0800

    Minor fix of `xy-emacs-build.sh'

--- bin/lin64/xy-emacs-buid.sh
+++ bin/lin64/xy-emacs-buid.sh
@@ -10,6 +10,23 @@ tar -zxvf ~/.emacs.d/backup/cedet-1.1.tar.gz
 mv cedet-1.1 cedet
 cd ~/.emacs.d/lisps/cedet
 emacs -nw -Q -l "cedet-build.el" -f cedet-build -f save-buffers-kill-terminal
-emacs -f xy/emacs-build -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
-emacs -f xy/emacs-build -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
-emacs -f xy/emacs-build -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
+cd ~/.emacs.d
+emacs -nw -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
+emacs -nw -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
+emacs -nw -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
+cd ~/.emacs.d/git-lisps/anything-config/
+git checkout master
+cd ~/.emacs.d/git-lisps/o-blog/
+git checkout master
+cd ~/.emacs.d/git-lisps/o-blog/bootstrap/
+git checkout master
+cd ~/.emacs.d/git-lisps/o-blog/Font-Awesome/
+git checkout master
+cd ~/.emacs.d/git-lisps/o-blog/less.js/
+git checkout master
+cd ~/.emacs.d/git-lisps/org2blog/
+git checkout master
+cd ~/.emacs.d
+git submodule sync
+cd ~/
+emacs -daemon
