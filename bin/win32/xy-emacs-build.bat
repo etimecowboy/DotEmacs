bc99c2d26dc6c3bde7c56bb292c8764d859eb967
Xin Yang <Xin2.Yang@gmail.com>
2012-08-13 07:56:19 +0800

    Add comment in `xy-emacs-build.bat' to remind user starting bash.exe

--- bin/win32/xy-emacs-build.bat
+++ bin/win32/xy-emacs-build.bat
@@ -1,30 +1,31 @@
-cd %HOME%/.emacs.d
+REM start bash shell before your run
+cd ~/.emacs.d
 git submodule init
 git submodule update
-cd  %HOME%/.emacs.d/git-lisps/o-blog/
+cd  ~/.emacs.d/git-lisps/o-blog/
 git submodule init
 git submodule update
-cd %HOME%/.emacs.d/lisps
-tar -zxvf %HOME%/.emacs.d/backup/cedet-1.1.tar.gz
+cd ~/.emacs.d/lisps
+tar -zxvf ~/.emacs.d/backup/cedet-1.1.tar.gz
 mv cedet-1.1 cedet
-cd %HOME%/.emacs.d/lisps/cedet
+cd ~/.emacs.d/lisps/cedet
 emacs -nw -Q -l "cedet-build.el" -f cedet-build -f save-buffers-kill-terminal
-cd %HOME%/.emacs.d
-emacs -nw -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
-emacs -nw -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
-emacs -nw -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
-cd %HOME%/.emacs.d/git-lisps/anything-config/
+cd ~/.emacs.d
+emacs -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
+emacs -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
+emacs -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
+cd ~/.emacs.d/git-lisps/anything-config/
 git checkout master
-cd %HOME%/.emacs.d/git-lisps/o-blog/
+cd ~/.emacs.d/git-lisps/o-blog/
 git checkout master
-cd %HOME%/.emacs.d/git-lisps/o-blog/bootstrap/
+cd ~/.emacs.d/git-lisps/o-blog/bootstrap/
 git checkout master
-cd %HOME%/.emacs.d/git-lisps/o-blog/Font-Awesome/
+cd ~/.emacs.d/git-lisps/o-blog/Font-Awesome/
 git checkout master
-cd %HOME%/.emacs.d/git-lisps/o-blog/less.js/
+cd ~/.emacs.d/git-lisps/o-blog/less.js/
 git checkout master
-cd %HOME%/.emacs.d/git-lisps/org2blog/
+cd ~/.emacs.d/git-lisps/org2blog/
 git checkout master
-cd %HOME%/.emacs.d
+cd ~/.emacs.d
 git submodule sync
-cd %HOME%/
+cd ~/
