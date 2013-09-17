a7533d40cc24ac8a20911097d3c8e9fe8aae97db
Xin Yang <Xin2.Yang@gmail.com>
2013-09-17 14:33:21 +0100

    Minor changes of make-my-emacs.bat

--- bin/win32/make-my-emacs.bat
+++ bin/win32/make-my-emacs.bat
@@ -1,14 +1,12 @@
-REM start bash shell before your run
 cd ~/.emacs.d
 git submodule init
 git submodule update
 cd ~/.emacs.d/lisps
-tar -zxvf ~/.emacs.d/backup/cedet-1.1.tar.gz
-cd ~/.emacs.d/lisps/cedet-1.1
-emacs -nw -Q -l "cedet-build.el" -f cedet-build -f save-buffers-kill-terminal
-cd ~/.emacs.d
-git submodule sync
+# tar -zxvf ~/.emacs.d/backup/cedet-1.1.tar.gz
+# cd ~/.emacs.d/lisps/cedet-1.1
+# emacs -nw -Q -l "cedet-build.el" -f cedet-build -f save-buffers-kill-terminal
+# cd ~/.emacs.d
+# git submodule sync
 cd ~/
 emacs -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
 emacs -f xy/emacs-build -f xy/emacs-build -f save-buffers-kill-terminal
-cd ~/
