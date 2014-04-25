5fdac016bb90bd4a60e86c7aac93af236f1ac8c6
Xin Yang <xin@mint>
2014-04-25 22:08:48 +0100

    * my-lisps/init/xy-rcroot-filemgr.el: minor change

--- my-lisps/init/xy-rcroot-filemgr.el
+++ my-lisps/init/xy-rcroot-filemgr.el
@@ -129,7 +129,8 @@
 ;; Use a single frame for visiting a sub-directory
 ;; (eval-after-load "dired-single" (dired-single-postload))
 ;; `T' 把目录压缩为.tar.gz文件
-;; (require 'dired-tar)
+;; (GNULinux
+;;  (try-require 'dired-tar))
 
 ;; open-with, open files in external applications
 ;; NOTE: it need to be patched in order to run on windows
